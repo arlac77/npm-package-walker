a2087e9b2498ead02f513ceaf14641e484c6a2ac
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 00:19:27 +0200

    chore(package): add rollup-plugin-commonjs@^9.1.3 chore(package): add rollup-plugin-node-resolve@^3.3.0 chore(package): add rollup-plugin-multi-entry@^2.0.2 chore(package): add rollup-plugin-istanbul@^2.0.1 chore(package): add rollup-plugin-executable@^1.2.0 chore(rollup): import resolve chore(rollup): import commonjs chore(rollup): import executable chore(rollup): import resolve chore(rollup): import commonjs chore(rollup): import multiEntry chore(rollup): import istanbul chore(rollup): add multiEntry to plugins chore(rollup): add istanbul to plugins chore(rollup): add resolve to plugins chore(rollup): add commonjs to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,3 +1,8 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import multiEntry from 'rollup-plugin-multi-entry';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 export default {
   input: 'tests/package-walker-test.js',
   output: {
